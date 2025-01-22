59cbe91e08f0f4b7438496cdf809d3ef610797d2
KfirFed <xd.KFederovsky@idf.il>
2025-01-22 20:44:10 +0200

    feat/check-win

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -64,4 +64,28 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun checkWin(): String {
+
+        if (gameBoard[0][0] == gameBoard[1][1] && gameBoard[0][0] == gameBoard[2][2]
+            && gameBoard[0][0] !== ""
+        ) return gameBoard[0][0]
+        if (gameBoard[0][2] == gameBoard[1][1] && gameBoard[0][2] == gameBoard[2][0]
+            && gameBoard[0][2] !== ""
+        ) return gameBoard[0][2]
+
+
+        for (i in 0..2) {
+            if (gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][0] == gameBoard[i][2]
+                && gameBoard[i][0] !== ""
+            ) return gameBoard[i][0]
+            if (gameBoard[0][i] == gameBoard[1][i] && gameBoard[0][i] == gameBoard[2][i]
+                && gameBoard[0][i] !== ""
+            ) return gameBoard[0][i]
+        }
+
+        return ""
+    }
+
+
 }
