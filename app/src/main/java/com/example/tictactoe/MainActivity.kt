7a6577968352897ea308fa1077ba45a35a9dd206
KfirFed <xd.KFederovsky@idf.il>
2025-01-22 23:56:16 +0200

    fixes

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -9,22 +9,22 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    var currentPlayer = "X"
-    var winMessage: TextView? = null
-    var currentPlayerText: TextView? = null
-    var endGame: Boolean = false
+    private var currentPlayer = "X"
+    private lateinit var winMessage: TextView
 
-    var position1: Button? = null
-    var position2: Button? = null
-    var position3: Button? = null
-    var position4: Button? = null
-    var position5: Button? = null
-    var position6: Button? = null
-    var position7: Button? = null
-    var position8: Button? = null
-    var position9: Button? = null
+    private lateinit var position1: Button
+    private lateinit var position2: Button
+    private lateinit var position3: Button
+    private lateinit var position4: Button
+    private lateinit var position5: Button
+    private lateinit var position6: Button
+    private lateinit var position7: Button
+    private lateinit var position8: Button
+    private lateinit var position9: Button
 
-    var grid: Array<Array<Button?>>? = null
+    private lateinit var grid: Array<Array<Button>>
+
+    private lateinit var newGameButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +47,6 @@ class MainActivity : AppCompatActivity() {
         position9 = findViewById(R.id.button22)
 
         winMessage = findViewById(R.id.winMessage)
-        currentPlayerText = findViewById(R.id.currentPlayerTextView)
 
         grid = arrayOf(
             arrayOf(position1, position2, position3),
@@ -55,9 +54,10 @@ class MainActivity : AppCompatActivity() {
             arrayOf(position7, position8, position9)
         )
 
+
         disableAllCells()
 
-        val newGameButton: Button = findViewById(R.id.newGameButton)
+        newGameButton = findViewById(R.id.newGameButton)
 
         newGameButton.setOnClickListener {
             resetGame()
@@ -67,44 +67,50 @@ class MainActivity : AppCompatActivity() {
 
     private fun resetGame() {
         currentPlayer = "X"
-        currentPlayerText?.text = "Player X turn"
+        newGameButton.isEnabled = false
+        newGameButton.text = "Player X turn"
+        winMessage.text = ""
 
         for (i in 0..2) {
             for (j in 0..2) {
-                grid!![i][j]?.text = ""
-                grid!![i][j]?.isEnabled = true
-                grid!![i][j]?.setOnClickListener {
-                    makeMove(grid!![i][j])
+                grid[i][j].text = ""
+                grid[i][j].isEnabled = true
+                grid[i][j].setOnClickListener {
+                    makeMove(grid[i][j])
                 }
             }
         }
     }
 
-    private fun makeMove(cell: Button?){
-        cell?.text = currentPlayer
-        cell?.isEnabled = false
+    private fun makeMove(cell: Button){
+        cell.text = currentPlayer
+        cell.isEnabled = false
         val winner = checkWin()
 
         if (winner == "") {
             val isTie = checkTie()
             if (isTie) {
-                winMessage?.text = "It's a tie!"
-                endGame = true
+                winMessage.text = "It's a tie!"
+                newGameButton.text = "New Game"
+                newGameButton.isEnabled = true
+                return
             }
         } else if (winner == "X" || winner == "O"){
             disableAllCells()
             winMessage(winner)
-            endGame = true
-        } else endGame = false
+            newGameButton.text = "New Game"
+            newGameButton.isEnabled = true
+            return
+        }
 
         currentPlayer = if (currentPlayer == "X") "O" else "X"
-        currentPlayerText?.text = "Player $currentPlayer's turn"
+        newGameButton.text = "Player $currentPlayer's turn"
     }
 
     private fun checkTie(): Boolean {
         for (i in 0..2){
             for (j in 0..2) {
-                if (grid!![i][j]?.text.toString() == "") return false
+                if (grid[i][j].text.toString() == "") return false
             }
         }
 
@@ -112,35 +118,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkWin(): String {
-        if (grid!![0][0]?.text == grid!![1][1]?.text && grid!![0][0]?.text == grid!![2][2]?.text
-            && grid!![0][0]?.text !== ""
-        ) return grid!![0][0]?.text.toString()
-        if (grid!![0][2]?.text == grid!![1][1]?.text && grid!![0][2]?.text == grid!![2][0]?.text
-            && grid!![0][2]?.text !== ""
-        ) return grid!![0][2]?.text.toString()
+        if (grid[0][0].text == grid[1][1].text && grid[0][0].text == grid[2][2].text
+            && grid[0][0].text !== ""
+        ) return grid[0][0].text.toString()
+        if (grid[0][2].text == grid[1][1].text && grid[0][2].text == grid[2][0].text
+            && grid[0][2].text !== ""
+        ) return grid[0][2].text.toString()
 
 
         for (i in 0..2) {
-            if (grid!![i][0]?.text == grid!![i][1]?.text && grid!![i][0]?.text == grid!![i][2]?.text
-                && grid!![i][0]?.text !== ""
-            ) return grid!![i][0]?.text.toString()
-            if (grid!![0][i]?.text == grid!![1][i]?.text && grid!![0][i]?.text == grid!![2][i]?.text
-                && grid!![0][i]?.text !== ""
-            ) return grid!![0][i]?.text.toString()
+            if (grid[i][0].text == grid[i][1].text && grid[i][0].text == grid[i][2].text
+                && grid[i][0].text !== ""
+            ) return grid[i][0].text.toString()
+            if (grid[0][i].text == grid[1][i].text && grid[0][i].text == grid[2][i].text
+                && grid[0][i].text !== ""
+            ) return grid[0][i].text.toString()
         }
 
         return ""
     }
 
     private fun winMessage(winChar: String) {
-        if (winChar === "X") winMessage?.text = "Player X win!"
-        else winMessage?.text = "Player O win!"
+        if (winChar === "X") winMessage.text = "Player X win!"
+        else winMessage.text = "Player O win!"
     }
 
     private fun disableAllCells() {
         for (i in 0..2) {
             for (j in 0..2) {
-                grid!![i][j]?.isEnabled = false
+                grid[i][j].isEnabled = false
             }
         }
     }
